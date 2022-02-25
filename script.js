e2c59ff2f196d349a3be556f3b40fdd321d5cff9
Andrew Moody <77068588+andrewmoody96@users.noreply.github.com>
2022-02-24 19:39:09 -0600

    Fixed submit and prevented default.

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
-var submitButton = document.querySelector("submit");
+var 
+var submitButton = document.querySelector("#submit");
 // Array with all possible questions.
 var possibleQuestions = ["Inside which HTML element do we put our JavaScript?", "Which code example will display 'Hello World' in an alert?", "How do you call a function named 'myFunction'?", "How do you write an IF statement to execute code when 'i' is not equal to 5?", "How do you create a function in JavaScript?"]
-
+// var question0Answers = 
 // provides random question from array of questions
-function randomQuestion(submit) {
-    submit.preventDefault()
-    console.log("Clicked");
-    
+
+function randomQuestion(e) {
+    e.preventDefault();
+        
 }
 
 // Click submit to generate the next random question. Then display that question.
 // need to preventDefault
-submitButton.addEventListener("submit", randomQuestion);
+submitButton.addEventListener("click", randomQuestion)
+    console.log("Response submitted.")
+;
