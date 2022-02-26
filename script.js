cdb2ede2609d49ad46cea188d1c0198c71051ad4
Andrew Moody <77068588+andrewmoody96@users.noreply.github.com>
2022-02-26 17:08:51 -0600

    Removed start quiz button

--- script.js
+++ script.js
@@ -1,7 +1,76 @@
+// Functions:
+
+// Starts quiz and populates the quiz with questions. 
+function createQuiz() {
+  console.log("Clicked");
+  // Stores question output
+  document.getElementById("selectYourAnswer-text").style.display = "block";
+  document.getElementById("quiz-form").style.display = "block";
+  let question = [];
+
+  // Show submit ONCE -- Place outside of loop
+  myQuestions.forEach((currentQuestion, questionNumber) => {
+    // Stores list of answers.
+    const answers = [];
+
+    for (letter in currentQuestion.answers) {
+      answers.push(
+        `<br><label>
+            <input type="radio" name="question${questionNumber}" value="${letter}">
+            ${letter} :
+            ${currentQuestion.answers[letter]}
+          </label>`
+      );
+    }
+    question.push(
+      `<div class="slide">
+        <div class ="question"> ${currentQuestion.question}</div>
+        <div class="answers"> ${answers.join("")}</div
+      </div>`
+    );
+  });
+  quizContainer.innerHTML = question.join("");
+  quizContainer.insertAdjacentHTML(
+    "beforeend",
+    `<button id="prevQ">Previous Question</button><button id="nextQ">Next Question</button>`
+  );  
+}
+
+// Logs results & MAYBE saves them to local storage.
+function logResults() {
+  console.log("Starting to log.");
+  const correctAnswers = currentQuestion.correctAnswer;
+  // const allAnswers = quizContainer.querySelectorAll(".answers");
+  // keeps track of user's correct answers
+  let countCorrect = 0;
+  let countIncorrect = 0;
+  myQuestions.forEach((currentQuestion, questionNumber) => {
+    // finds user's selected answer
+    var answerContainer = allAnswers[questionNumber];
+    var selector = `input[name=question${questionNumber}]:checked`;
+    var userAnswer = (answerContainer.querySelector(selector) || {}).value;
+    // checks if answer is correct
+    if (userAnswer === currentQuestion.correctAnswer) {
+      numCorrect++;
+    } else {
+      numIncorrect++;
+    }
+  });
+}
+
+function showQuestion()
+
+function showNext()
+
+function showPrev()
+
 var startButton = document.querySelector("#startButton");
 var quizContainer = document.querySelector("#quiz");
+var prevBtn = document.querySelector("#previous");
+var nextBtn = document.querySelector("#next");
+var submitBtn = document.querySelector("#submit");
 var scoreContainer = document.querySelector("#score");
-var submitButton = document.querySelector("#submit");
+
 // var quizForm = document.querySelector("#quiz-form");
 
 // Object containing all possible questions.
@@ -9,9 +78,9 @@ const myQuestions = [
   {
     question: "Inside which HTML element do we put our JavaScript?",
     answers: {
-      a: "Answer 1",
-      b: "Answer 2",
-      c: "Answer 3",
+      a: "head",
+      b: "p",
+      c: "body",
     },
     correctAnswer: "c",
   },
@@ -56,66 +125,18 @@ const myQuestions = [
 // Need to display a new question value from possibleQuestions on every click.
 var currentQuestion = "";
 
-// Make sure that question is displayed before the answers. This is ugly right now but it WORKS.
-function createQuiz() {
-  console.log("Clicked");
-  // Stores question output
-  document.getElementById("selectYourAnswer-text").style.display = "block";
-  document.getElementById("quiz-form").style.display = "block";
-  let question = [];
-
-  // Show submit ONCE -- Place outside of loop
-  myQuestions.forEach((currentQuestion, questionNumber) => {
-    // Stores list of answers.
-    const answers = [];
-
-    for (letter in currentQuestion.answers) {
-      answers.push(
-        `<br><label>
-            <input type="radio" name="question${questionNumber}" value="${letter}">
-            ${letter} :
-            ${currentQuestion.answers[letter]}
-          </label>`
-      );
-    }
-    question.push(
-      `<div class ="question"> ${currentQuestion.question}</div>
-          <div class="answers"> ${answers.join("")}</div`
-    );
-  });
-  quizContainer.innerHTML = question.join("");
-  // quizForm.innerHTML = `<input type="submit" name="submitAnswer" id="submit" value="Submit Answer"></input>`;
-  quizContainer.insertAdjacentHTML(
-    "beforeend",
-    `<button type="submit" id="submit">Submit Answers</button>`
-  );  
-}
 
 
 
-function logResults() {
-  console.log("Starting to log.");
-  const correctAnswers = currentQuestion.correctAnswer;
-  // const allAnswers = quizContainer.querySelectorAll(".answers");
-  // keeps track of user's correct answers
-  let countCorrect = 0;
-  let countIncorrect = 0;
-  myQuestions.forEach((currentQuestion, questionNumber) => {
-    // finds user's selected answer
-    var answerContainer = allAnswers[questionNumber];
-    var selector = `input[name=question${questionNumber}]:checked`;
-    var userAnswer = (answerContainer.querySelector(selector) || {}).value;
-    // checks if answer is correct
-    if (userAnswer === currentQuestion.correctAnswer) {
-      numCorrect++;
-    } else {
-      numIncorrect++;
-    }
-  });
-}
 
-// Starts quiz.
-startButton.addEventListener("click", createQuiz);
+// Loads quiz when page loads.
+createQuiz();
 
 // Click submit to generate the next random question. Then display that question.
-submitButton.addEventListener("submit", logResults);
+
+
+// // Moves to next question.
+// nextQuestion.addEventListener("click", advanceSlide);
+
+// // Moves to previous question.
+// prevQuestion.addEventListener("click", prevSlide);
