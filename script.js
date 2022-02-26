2ff4622c5f876cc613c6caed5ccd6953f43ce2a0
Andrew Moody <77068588+andrewmoody96@users.noreply.github.com>
2022-02-25 22:38:16 -0600

    Displays single question. Will add more.

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 var startButton = document.querySelector("#startButton");
 var quizContainer = document.querySelector("#quiz");
+var scoreContainer = document.querySelector("#score");
 var submitButton = document.querySelector("#submit");
-
+// var quizForm = document.querySelector("#quiz-form");
 
 // Object containing all possible questions.
 const myQuestions = [
@@ -10,53 +11,52 @@ const myQuestions = [
     answers: {
       a: "Answer 1",
       b: "Answer 2",
-      c: "Answer 3"
+      c: "Answer 3",
     },
-    correctAnswer: "c"
+    correctAnswer: "c",
   },
-  {
-    question: "Which code example will display 'Hello World' in an alert?",
-    answers: {
-      a: "Answer 1",
-      b: "Answer 2",
-      c: "Answer 3"
-    },
-    correctAnswer: "c"
-  },
-  {
-    question: "How do you call a function named 'myFunction'?",
-    answers: {
-      a: "Answer 1",
-      b: "Answer 2",
-      c: "Answer 3"
-    },
-    correctAnswer: "c"
-  },
-  {
-    question: "How do you write an IF statement to execute code when 'i' is not equal to 5?",
-    answers: {
-      a: "Answer 1",
-      b: "Answer 2",
-      c: "Answer 3"
-    },
-    correctAnswer: "c"
-  },
-  {
-    question: "How do you create a function in JavaScript?",
-    answers: {
-      a: "Answer 1",
-      b: "Answer 2",
-      c: "Answer 3"
-    },
-    correctAnswer: "c"
-  },  
+  // {
+  //   question: "Which code example will display 'Hello World' in an alert?",
+  //   answers: {
+  //     a: "Answer 1",
+  //     b: "Answer 2",
+  //     c: "Answer 3",
+  //   },
+  //   correctAnswer: "c",
+  // },
+  // {
+  //   question: "How do you call a function named 'myFunction'?",
+  //   answers: {
+  //     a: "Answer 1",
+  //     b: "Answer 2",
+  //     c: "Answer 3",
+  //   },
+  //   correctAnswer: "c",
+  // },
+  // {
+  //   question:
+  //     "How do you write an IF statement to execute code when 'i' is not equal to 5?",
+  //   answers: {
+  //     a: "Answer 1",
+  //     b: "Answer 2",
+  //     c: "Answer 3",
+  //   },
+  //   correctAnswer: "c",
+  // },
+  // {
+  //   question: "How do you create a function in JavaScript?",
+  //   answers: {
+  //     a: "Answer 1",
+  //     b: "Answer 2",
+  //     c: "Answer 3",
+  //   },
+  //   correctAnswer: "c",
+  // },
 ];
-// Need to display a new question value from possibleQuestions on every click. 
+// Need to display a new question value from possibleQuestions on every click.
 var currentQuestion = "";
-// push the displayed index into alreadyAsked and remove it from possibleQuestions
-var alreadyAsked = [];
 
-// Make sure that question is displayed before the answers. This is ugly right now but it WORKS. 
+// Make sure that question is displayed before the answers. This is ugly right now but it WORKS.
 function createQuiz() {
   console.log("Clicked");
   // Stores question output
@@ -66,42 +66,49 @@ function createQuiz() {
 
   // Show submit ONCE -- Place outside of loop
   myQuestions.forEach((currentQuestion, questionNumber) => {
-      // Stores list of answers.
-      const answers = [];
-      
-      for(letter in currentQuestion.answers){
-        answers.push(
-          `<label><input type="radio" name="question${questionNumber}" value="${letter}">
-          ${letter} :
-          ${currentQuestion.answers[letter]}
+    // Stores list of answers.
+    const answers = [];
+
+    for (letter in currentQuestion.answers) {
+      answers.push(
+        `<br><label>
+            <input type="radio" name="question${questionNumber}" value="${letter}">
+            ${letter} :
+            ${currentQuestion.answers[letter]}
           </label>`
-          );
-        }
-        question.push(
-          `<div class ="question"> ${currentQuestion.question}</div>
-          <div class="answers"> ${answers.join('')}</div`
-        );
-      }  
+      );
+    }
+    question.push(
+      `<div class ="question"> ${currentQuestion.question}</div>
+          <div class="answers"> ${answers.join("")}</div`
+    );
+  });
+  quizContainer.innerHTML = question.join("");
+  // quizForm.innerHTML = `<input type="submit" name="submitAnswer" id="submit" value="Submit Answer"></input>`;
+  quizContainer.insertAdjacentHTML(
+    "beforeend",
+    `<button type="submit" id="submit"></button>`
   );
-  quizContainer.innerHTML = question.join('');
-
-  console.log(question.toString);
-
-
-//   currentQuestion = possibleQuestions.forEach()
-//   for (let i = 0; i <= 4; i++) {
-//       console.log(currentQuestion)
-//       currentQuestion = possibleQuestions[i];
-//   }
-//   console.log(currentQuestion);
   
-  
-//   document.getElementById("#questions");
-//   currentQuestion.append(randomQuestion);
+  // console.log(currentQuestion);
+
+  //   currentQuestion = possibleQuestions.forEach()
+  //   for (let i = 0; i <= 4; i++) {
+  //       console.log(currentQuestion)
+  //       currentQuestion = possibleQuestions[i];
+  //   }
+  //   console.log(currentQuestion);
+
+  //   document.getElementById("#questions");
+  //   currentQuestion.append(randomQuestion);
 }
 
+
+
 function logResults() {
-  const allAnswers = quizContainer.querySelectorAll('.answers');
+  console.log("Starting to log.");
+  const correctAnswers = currentQuestion.correctAnswer;
+  // const allAnswers = quizContainer.querySelectorAll(".answers");
   // keeps track of user's correct answers
   let countCorrect = 0;
   let countIncorrect = 0;
@@ -110,18 +117,17 @@ function logResults() {
     var answerContainer = allAnswers[questionNumber];
     var selector = `input[name=question${questionNumber}]:checked`;
     var userAnswer = (answerContainer.querySelector(selector) || {}).value;
-
     // checks if answer is correct
-    if(userAnswer === currentQuestion.correctAnswer){
+    if (userAnswer === currentQuestion.correctAnswer) {
       numCorrect++;
     } else {
       numIncorrect++;
     }
-  })
+  });
 }
 
 // Starts quiz.
 startButton.addEventListener("click", createQuiz);
 
 // Click submit to generate the next random question. Then display that question.
-// submitButton.addEventListener("click", displayQuestion);
+submitButton.addEventListener("submit", logResults);
