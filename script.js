757f92a40399f2a68588115f2d84e5ce30d3ae62
Andrew Moody <77068588+andrewmoody96@users.noreply.github.com>
2022-02-26 20:18:49 -0600

    getElementID but didn't use a #. We <3 progress

--- script.js
+++ script.js
@@ -1,14 +1,10 @@
 // Functions:
-
 // Starts quiz and populates the quiz with questions. 
 function createQuiz() {
-  console.log("Clicked");
+  console.log("Clicked");  
   // Stores question output
-  document.getElementById("selectYourAnswer-text").style.display = "block";
-  document.getElementById("quiz-form").style.display = "block";
-  let question = [];
+  const output = [];
 
-  // Show submit ONCE -- Place outside of loop
   myQuestions.forEach((currentQuestion, questionNumber) => {
     // Stores list of answers.
     const answers = [];
@@ -22,59 +18,74 @@ function createQuiz() {
           </label>`
       );
     }
-    question.push(
+    output.push(
       `<div class="slide">
-        <div class ="question"> ${currentQuestion.question}</div>
-        <div class="answers"> ${answers.join("")}</div
+        <div class="question"> ${currentQuestion.question} </div>
+        <div class="answers"> ${answers.join("")}</div>
       </div>`
     );
   });
-  quizContainer.innerHTML = question.join("");
-  quizContainer.insertAdjacentHTML(
-    "beforeend",
-    `<button id="prevQ">Previous Question</button><button id="nextQ">Next Question</button>`
-  );  
-}
+  quizContainer.innerHTML = output.join(""); 
+};
 
-// Logs results & MAYBE saves them to local storage.
+// Logs results to console.
 function logResults() {
   console.log("Starting to log.");
-  const correctAnswers = currentQuestion.correctAnswer;
-  // const allAnswers = quizContainer.querySelectorAll(".answers");
+  const answerContainers = quizContainer.querySelectorAll(".answers");
+
   // keeps track of user's correct answers
   let countCorrect = 0;
-  let countIncorrect = 0;
+
   myQuestions.forEach((currentQuestion, questionNumber) => {
     // finds user's selected answer
-    var answerContainer = allAnswers[questionNumber];
-    var selector = `input[name=question${questionNumber}]:checked`;
-    var userAnswer = (answerContainer.querySelector(selector) || {}).value;
+    const answerContainer = answerContainers[questionNumber];
+    const selector = `input[name=question${questionNumber}]:checked`;
+    const userAnswer = (answerContainer.querySelector(selector) || {}).value;
+    
     // checks if answer is correct
     if (userAnswer === currentQuestion.correctAnswer) {
-      numCorrect++;
+      countCorrect++;
+      console.log("Good job.");
     } else {
-      numIncorrect++;
+      console.log("That ain't it.");
     }
   });
+
+  var results = countCorrect;
+  console.log(results);
+};
+
+function showSlide(n) {
+  slides[currentSlide].classList.remove("active-slide");
+  slides[n].classList.add("active-slide");
+  currentSlide = n;
+  if (currentSlide === 0) {
+    prevBtn.style.display = "none";
+  } else {
+    prevBtn.style.display = "inline-block";
+  }
+  if (currentSlide === slides.length - 1) {
+    nextBtn.style.display = "none";
+    submitBtn.style.display = "inline-block";
+  } else {
+    nextBtn.style.display = "inline-block";
+    submitBtn.style.display = "none";
+  }
+};
+
+function showNext() {
+  showSlide(currentSlide + 1);
 }
 
-function showQuestion()
+function showPrev() {
+  showSlide(currentSlide - 1);
+}
 
-function showNext()
-
-function showPrev()
-
-var startButton = document.querySelector("#startButton");
-var quizContainer = document.querySelector("#quiz");
-var prevBtn = document.querySelector("#previous");
-var nextBtn = document.querySelector("#next");
-var submitBtn = document.querySelector("#submit");
-var scoreContainer = document.querySelector("#score");
-
-// var quizForm = document.querySelector("#quiz-form");
-
-// Object containing all possible questions.
+const quizContainer = document.getElementById("quiz");
+const scoreContainer = document.getElementById("score");
+const submitBtn = document.getElementById("submit");
 const myQuestions = [
+// Object containing all possible questions.
   {
     question: "Inside which HTML element do we put our JavaScript?",
     answers: {
@@ -84,59 +95,64 @@ const myQuestions = [
     },
     correctAnswer: "c",
   },
-  // {
-  //   question: "Which code example will display 'Hello World' in an alert?",
-  //   answers: {
-  //     a: "Answer 1",
-  //     b: "Answer 2",
-  //     c: "Answer 3",
-  //   },
-  //   correctAnswer: "c",
-  // },
-  // {
-  //   question: "How do you call a function named 'myFunction'?",
-  //   answers: {
-  //     a: "Answer 1",
-  //     b: "Answer 2",
-  //     c: "Answer 3",
-  //   },
-  //   correctAnswer: "c",
-  // },
-  // {
-  //   question:
-  //     "How do you write an IF statement to execute code when 'i' is not equal to 5?",
-  //   answers: {
-  //     a: "Answer 1",
-  //     b: "Answer 2",
-  //     c: "Answer 3",
-  //   },
-  //   correctAnswer: "c",
-  // },
-  // {
-  //   question: "How do you create a function in JavaScript?",
-  //   answers: {
-  //     a: "Answer 1",
-  //     b: "Answer 2",
-  //     c: "Answer 3",
-  //   },
-  //   correctAnswer: "c",
-  // },
+  {
+    question: "Which code example will display 'Hello World' in an alert?",
+    answers: {
+      a: "Answer 1",
+      b: "Answer 2",
+      c: "Answer 3",
+    },
+    correctAnswer: "c",
+  },
+  {
+    question: "How do you call a function named 'myFunction'?",
+    answers: {
+      a: "Answer 1",
+      b: "Answer 2",
+      c: "Answer 3",
+    },
+    correctAnswer: "c",
+  },
+  {
+    question:
+      "How do you write an IF statement to execute code when 'i' is not equal to 5?",
+    answers: {
+      a: "Answer 1",
+      b: "Answer 2",
+      c: "Answer 3",
+    },
+    correctAnswer: "c",
+  },
+  {
+    question: "How do you create a function in JavaScript?",
+    answers: {
+      a: "Answer 1",
+      b: "Answer 2",
+      c: "Answer 3",
+    },
+    correctAnswer: "c",
+  },
 ];
-// Need to display a new question value from possibleQuestions on every click.
-var currentQuestion = "";
-
-
-
-
 
 // Loads quiz when page loads.
 createQuiz();
 
-// Click submit to generate the next random question. Then display that question.
+// Navigation and slides
+const prevBtn = document.getElementById("previous");
+const nextBtn = document.getElementById("next");
+const slides = document.querySelectorAll(".slide");
+let currentSlide = 0;
 
+// Displays first question's slide.
+showSlide(currentSlide);
 
-// // Moves to next question.
-// nextQuestion.addEventListener("click", advanceSlide);
+// Event Listeners
 
-// // Moves to previous question.
-// prevQuestion.addEventListener("click", prevSlide);
+// Submits answers at the end of the quiz.
+submitBtn.addEventListener("click", logResults)
+
+// Returns to the previous question.
+prevBtn.addEventListener("click", showPrev);
+
+// Advances to the next question.
+nextBtn.addEventListener("click", showNext);
