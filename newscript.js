89d784db842ff15ca6269b386ad739486574d41b
Andrew Moody <77068588+andrewmoody96@users.noreply.github.com>
2022-02-28 13:30:57 -0600

    Reviewing class videos and starting over.

--- newscript.js
+++ newscript.js
@@ -1,7 +1,7 @@
-// EVENT LISTENERS:
-// -------------------------------------------------------------------------------------
+
 // START THE QUIZ --- 
-// Start quiz upon click of a "start quiz" button. 
+// Start quiz upon click of a "start quiz" button. --- CHECK
+
 // As part of the quiz, also start a timer.
 // When the quiz starts, remove the "start quiz" button.
 
@@ -14,10 +14,71 @@
 // Display a text input to store user's initials and score in localStorage.
 // After clicking submit, display "Scores" and the last user's initials and score on the page. 
 
+function timer() {
 
+}
+
+function theQuiz() {
+    console.log("Quiz Started")
+    setInterval
+};
 
 
 // VARIABLES
-var = startBtn 
-var = submitBtn
+var startBtn = document.getElementById("startBtn");
+var submitBtn = document.getElementById("submitBtn");
+var scoresBtn = document.getElementById("scoresBtn");
+// object containing all questions
+const myQuestions = [
+    {
+      question: "Inside which HTML element do we put our JavaScript?",
+      answers: {
+        a: "head",
+        b: "p",
+        c: "body",
+      },
+      correctAnswer: "c",
+    },
+    {
+      question: "Which code example will display 'Hello World' in an alert?",
+      answers: {
+        a: "alert(\"Hello World\")",
+        b: "display(\"Hello World\")",
+        c: "alert: \"Hello World\"",
+      },
+      correctAnswer: "a",
+    },
+    {
+      question: "How do you call a function named 'myFunction'?",
+      answers: {
+        a: "call {myFunction()}",
+        b: "myFunction()",
+        c: "call function myFunction()",
+      },
+      correctAnswer: "b",
+    },
+    {
+      question:
+        "How do you write an IF statement to execute code when 'i' is not equal to 5?",
+      answers: {
+        a: "if [i !=5]",
+        b: "if (i != 5)",
+        c: "if i(!= 5)",
+      },
+      correctAnswer: "b",
+    },
+    {
+      question: "How do you create a function in JavaScript?",
+      answers: {
+        a: "function myFunction()",
+        b: "function = myFunction()",
+        c: "function:myFunction()",
+      },
+      correctAnswer: "a",
+    },
+  ];
 
+
+// EVENT LISTENERS:
+// -------------------------------------------------------------------------------------
+startBtn.addEventListener("click", theQuiz);
